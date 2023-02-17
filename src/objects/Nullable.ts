5b3fd656d24734168b72ce9cf0e505dbf3e2ea80
mikolaszko <mikosodz.lote@gmail.com>
2023-02-17 22:45:52 +0100

    Nulable/NullableExcept

---
+++ src/objects/Nullable.ts
@@ -0,0 +1,6 @@
+// Description:
+// simple utility type to make all properties of an object nullable
+// that uses the keyof operator to get all the keys of the object
+// and then uses the in operator to iterate over all the keys
+
+export type Nullable<T> = { [K in keyof T]: T[K] | null }
---
+++ src/objects/NullableExcept.ts
@@ -0,0 +1,15 @@
+// Description:
+// simple utility type to make all properties of an object nullable
+// except for the ones specified in the generic type K
+
+export type NullableExcept<T, K extends keyof T> = {
+	[P in keyof T]: P extends K ? T[P] : T[P] | null
+}
+
+type test = {
+	name: string
+	age: number
+	address: string
+}
+
+type nulledType = NullableExcept<test, "name" | "age">
